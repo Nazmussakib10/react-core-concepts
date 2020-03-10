76923e03bb535c208c89f435216facb0a737646a
Nazmussakib10 <sakib.cse133@gmail.com>
2020-03-11 02:35:12 +0600

    some react practice

--- src/App.js
+++ src/App.js
@@ -3,6 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person={
+    name:"Nazmus sakib",
+    profession:"web Developer"
+  }
+  var person2={
+    name:"salman shah",
+    profession:"banglar famous hero"
+  }
+  var style={
+    color:"blue",
+    backgroundColor:"white",
+    fontSize:"20px",
+    padding:'30px',
+    margin:"10px"
+
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,9 +28,17 @@ function App() {
         <p>
           Edit done <code>src/App.js</code> and save to reload.
         </p>
+        <h1 style={style}>My credential is : {person.name+ " is a "+person.profession}</h1>
+  
+        <h2 style={{color:"white",backgroundColor:'cyan',padding:"40px",margin:"20px"}}>Hero of Bangladesh : {person2.name+" "+person2.profession}</h2>
+       <a href="https://www.facebook.com/">my facebook link</a>
+        <p>my first paragraph</p>
+        <p>this is the paragraph that i written on her birthday</p>
+
       
       </header>
     </div>
+        
   );
 }
 
